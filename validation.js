c5cce3b5a531b41aea0041893394a70ac8b607af
zwospiel <johannes.seiferth@protonmail.com>
2022-09-25 16:57:44 +0200

    Implement input validation for parensAreBalanced

--- validation.js
+++ validation.js
@@ -2,6 +2,12 @@ function parensAreBalanced(formula) {
     if (typeof(formula) !== "string") {
         throw new TypeError("Expected input to be a string.")
     }
+
+    for (const character of formula) {
+        if (character != '(' && character != ')') {
+            throw new Error("Invalid input.")
+        }
+    }
 }
 
 
