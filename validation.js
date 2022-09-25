64cbe06d3858fbb10617629c38f8d6eaa6687e41
zwospiel <johannes.seiferth@protonmail.com>
2022-09-25 17:36:15 +0200

    Implement parensAreBalanced

--- validation.js
+++ validation.js
@@ -1,9 +1,20 @@
 function parensAreBalanced(formula) {
     validate(formula)
-    
+
     if (formula === "") {
         return true
     }
+
+    let count = 0
+    for (const character of formula) {
+        if (character == '(') {
+            count++
+        } else if (--count < 0) {
+            return false
+        }
+    }
+
+    return count == 0
 }
 
 function validate(formula) {
