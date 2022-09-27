fe531847db9efe10c1ea1cea974d765071d8a416
Christoph Aurich <dev.aurich@gmail.com>
2022-09-27 17:27:44 +0200

    Rename parens to brackets
    This is actually the correct umbrella term.
    A bracket can be one of these four:
    - (): round brackets or parentheses (parens in short)
    - []: square brackets or just brackets
    - {}: curly brackets or braces
    - <>: angled brackets or chevrons

--- validation.test.js
+++ validation.test.js
@@ -1,73 +1,73 @@
-const parensAreBalanced = require('./validation');
+const bracketsAreBalanced = require('./validation');
 
 
-describe('Expect parensAreBalanced to throw', () => {
+describe('Expect bracketsAreBalanced to throw', () => {
     describe('a TypeError', () => {
         test('for non-string inputs', () => {
-            expect(() => parensAreBalanced()).toThrow(TypeError)
-            expect(() => parensAreBalanced(4)).toThrow(TypeError)
-            expect(() => parensAreBalanced(['(', ')'])).toThrow(TypeError)
+            expect(() => bracketsAreBalanced()).toThrow(TypeError)
+            expect(() => bracketsAreBalanced(4)).toThrow(TypeError)
+            expect(() => bracketsAreBalanced(['(', ')'])).toThrow(TypeError)
         })
     })
     describe('an Error', () => {
         test('for inputs with invalid characters', () => {
-            expect(() => parensAreBalanced('4')).toThrow(Error)
-            expect(() => parensAreBalanced('(4)')).toThrow(Error)
-            expect(() => parensAreBalanced('(4+5)')).toThrow(Error)
+            expect(() => bracketsAreBalanced('4')).toThrow(Error)
+            expect(() => bracketsAreBalanced('(4)')).toThrow(Error)
+            expect(() => bracketsAreBalanced('(4+5)')).toThrow(Error)
         })
     })
     describe('nothing', () => {
         test('for balanced inputs without invalid characters', () => {
-            expect(() => parensAreBalanced('()')).not.toThrow()
+            expect(() => bracketsAreBalanced('()')).not.toThrow()
         })
         test('for unbalanced inputs without invalid characters', () => {
-            expect(() => parensAreBalanced(')(')).not.toThrow()
+            expect(() => bracketsAreBalanced(')(')).not.toThrow()
         })
     })
 })
 
-describe('Expect parensAreBalanced to return', () => {
+describe('Expect bracketsAreBalanced to return', () => {
     describe('true ', () => {
         test('for empty input', () => {
-            expect(parensAreBalanced('')).toBe(true)
+            expect(bracketsAreBalanced('')).toBe(true)
         })
-        test('for two balanced parens', () => {
-            expect(parensAreBalanced('()')).toBe(true)
+        test('for two balanced brackets', () => {
+            expect(bracketsAreBalanced('()')).toBe(true)
         })
-        test('for a nested pair of balanced parens', () => {
-            expect(parensAreBalanced('(())')).toBe(true)
+        test('for a nested pair of balanced brackets', () => {
+            expect(bracketsAreBalanced('(())')).toBe(true)
         })
-        test('for a sequential pair of balanced parens', () => {
-            expect(parensAreBalanced('()()')).toBe(true)
+        test('for a sequential pair of balanced brackets', () => {
+            expect(bracketsAreBalanced('()()')).toBe(true)
         })
     })
     describe('false', () => {
-        test('for an unclosed opening paren', () => {
-            expect(parensAreBalanced('(')).toBe(false)
+        test('for an unclosed opening bracket', () => {
+            expect(bracketsAreBalanced('(')).toBe(false)
         })
-        test('for an even number of unclosed opening parens', () => {
-            expect(parensAreBalanced('((')).toBe(false)
+        test('for an even number of unclosed opening brackets', () => {
+            expect(bracketsAreBalanced('((')).toBe(false)
         })
-        test('for a single unopened closing paren', () => {
-            expect(parensAreBalanced(')')).toBe(false)
+        test('for a single unopened closing bracket', () => {
+            expect(bracketsAreBalanced(')')).toBe(false)
         })
-        test('for an even number of unopened closing parens', () => {
-            expect(parensAreBalanced('))')).toBe(false)
+        test('for an even number of unopened closing brackets', () => {
+            expect(bracketsAreBalanced('))')).toBe(false)
         })
-        test('for a pair of opening and closed parens in wrong order', () => {
-            expect(parensAreBalanced(')(')).toBe(false)
+        test('for a pair of opening and closed brackets in wrong order', () => {
+            expect(bracketsAreBalanced(')(')).toBe(false)
         })
-        test('for an extra opening paren', () => {
-            expect(parensAreBalanced('()(')).toBe(false)
+        test('for an extra opening bracket', () => {
+            expect(bracketsAreBalanced('()(')).toBe(false)
         })
-        test('for an extra closing paren', () => {
-            expect(parensAreBalanced('())')).toBe(false)
+        test('for an extra closing bracket', () => {
+            expect(bracketsAreBalanced('())')).toBe(false)
         })
-        test('for a long formula with an unclosed opening paren in the middle', () => {
-            expect(parensAreBalanced('(())()()((())')).toBe(false)
+        test('for a long formula with an unclosed opening bracket in the middle', () => {
+            expect(bracketsAreBalanced('(())()()((())')).toBe(false)
         })
-        test('for a long formula with an unopened closing paren in the middle', () => {
-            expect(parensAreBalanced('(())()())(())')).toBe(false)
+        test('for a long formula with an unopened closing bracket in the middle', () => {
+            expect(bracketsAreBalanced('(())()())(())')).toBe(false)
         })
     })
 })
