261846c0c028e2c4dd2801f3689eae9365160a82
Christoph Aurich <dev.aurich@gmail.com>
2022-10-08 20:55:37 +0200

    Implement bitwise-addition algorithm

---
+++ js/operators.js
@@ -0,0 +1,22 @@
+const InvalidInput = require("./Errors")
+
+
+function plus(a, b) {
+    if (typeof(a) !== "number" | typeof(b) !== "number") {
+        throw new TypeError("Not a number.")
+    } else if (!(Number.isInteger(a) & Number.isInteger(b))) {
+        throw new InvalidInput("Not an integer.")
+    }
+
+    let carry = 0
+    while (b !== 0) {
+        carry = a & b
+        a = a ^ b
+        b = carry << 1
+    }
+
+    return a
+}
+
+
+module.exports = { plus }
