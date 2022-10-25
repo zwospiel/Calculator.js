72bd1c9cca91806776b07b54bcaaf731e58f4fb8
Christoph Aurich <dev.aurich@gmail.com>
2022-10-25 23:30:06 +0200

    Update Brackets
    * shorten some identifiers
    * add JSDocs

--- js/Brackets.js
+++ js/Brackets.js
@@ -1,31 +1,43 @@
 export class Brackets {
-    static #closedByOpeningBrackets = {
+    static #closedByOpen = {
         "(": ")",
         "[": "]",
         "{": "}",
     }
 
-    static get #openingBrackets() {
-        return Object.keys(Brackets.#closedByOpeningBrackets)
+    static get #openBrackets() {
+        return Object.keys(Brackets.#closedByOpen)
     }
 
-    static get #closingBrackets() {
-        return Object.values(Brackets.#closedByOpeningBrackets)
+    static get #closedBrackets() {
+        return Object.values(Brackets.#closedByOpen)
     }
 
     static get #validBrackets() {
-        return Brackets.#openingBrackets.concat(Brackets.#closingBrackets)
+        return Brackets.#openBrackets.concat(Brackets.#closedBrackets)
     }
 
-    static isOpeningBracket(character) {
-        return Brackets.#openingBrackets.includes(character)
+    /**
+     * @param {string} character
+     * @returns {boolean} True if the given character is an open bracket, false if not.
+     */
+    static isOpen(character) {
+        return Brackets.#openBrackets.includes(character)
     }
 
-    static getClosingBracketFor(openingBracket) {
-        return Brackets.#closedByOpeningBrackets[openingBracket]
+    /**
+     * @param {string} openBracket
+     * @returns {string} The matching closed bracket to the given open bracket.
+     */
+    static getClosed(openBracket) {
+        return Brackets.#closedByOpen[openBracket]
     }
 
-    static isValidBracket(character) {
+    /**
+     * @param {string} character
+     * @returns {boolean} True if the given character is a valid bracket, false if not.
+     */
+    static includes(character) {
         return Brackets.#validBrackets.includes(character)
     }
 }
