ee7aeb3d35d3604f02eb36701947f10400fd7e71
zwospiel <johannes.seiferth@protonmail.com>
2022-10-31 22:32:29 +0100

    Implement Integer.multiply

--- js/Integer.test.js
+++ js/Integer.test.js
@@ -37,6 +37,45 @@ describe("Integer.add", () => {
     })
 })
 
+describe("Integer.multiply", () => {
+    describe("returns correct product", () => {
+        test("for positive factors", () => {
+            expect(Integer.multiply(3, 4)).toBe(12)
+            expect(Integer.multiply(123, 456)).toBe(56088)
+        })
+        test("for negative factors", () => {
+            expect(Integer.multiply(3, -4)).toBe(-12)
+            expect(Integer.multiply(-5, 6)).toBe(-30)
+            expect(Integer.multiply(-8, -9)).toBe(72)
+        })
+        test("when multiplying by 0", () => {
+            expect(Integer.multiply(0, 4)).toBe(0)
+            expect(Integer.multiply(4, 0)).toBe(0)
+            expect(Integer.multiply(0, 0)).toBe(0)
+        })
+        test("when multiplying a negative number by 0", () => {
+            expect(Integer.multiply(0, -12)).toBe(0)
+            expect(Integer.multiply(0, -7)).toBe(0)
+        })
+    })
+    describe("throws", () => {
+        describe("a TypeError", () => {
+            test("for non-number inputs", () => {
+                expect(() => Integer.multiply("4", 3)).toThrow(TypeError)
+                expect(() => Integer.multiply(4, "3")).toThrow(TypeError)
+                expect(() => Integer.multiply([ 4 ], 3)).toThrow(TypeError)
+                expect(() => Integer.multiply(4, [ 3 ])).toThrow(TypeError)
+            })
+        })
+        describe("an InvalidInput error", () => {
+            test("for non-integer inputs", () => {
+                expect(() => Integer.multiply(4.1, 3)).toThrow(InvalidInput)
+                expect(() => Integer.multiply(4, 3.9)).toThrow(InvalidInput)
+            })
+        })
+    })
+})
+
 describe("Integer.negate", () => {
     describe("returns negated input", () => {
         test("for positive numbers", () => {
