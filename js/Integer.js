c96316d293789aee4a6070e24117f56aa7877c0c
zwospiel <johannes.seiferth@protonmail.com>
2022-10-31 22:14:51 +0100

    Implement Integer.negate

--- js/Integer.js
+++ js/Integer.js
@@ -4,6 +4,8 @@ import { InvalidInput } from "./Errors"
 // In this file we are manipulating Integers using the Two's Complement binary representation,
 // instead of the inbuilt JavaScript Number functionality.
 export class Integer {
+    static MIN = 1 << 31
+
     static add(a, b) {
         Integer.#validate(a, b)
 
@@ -15,6 +17,20 @@ export class Integer {
         }
 
         return a
+    }
+
+    static negate(integer) {
+        Integer.#validate(integer)
+        return Integer.#negate(integer)
+    }
+
+    static #negate(integer) {
+        return Integer.add(Integer.#flipBits(integer), 1)
+    }
+
+    static #flipBits(integer) {
+        // Flipping bits is equal to an XOR comparison with 1 for every bit of the input.
+        return integer ^ (Integer.MIN >> 31)
     }
 
     static #validate(...input) {
