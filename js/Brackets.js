eb5de9255e375668e1e284a648926c3fe0289a96
c-auri <43008483+c-auri@users.noreply.github.com>
2022-12-09 20:31:03 +0100

    Rewrite static classes to module functions
    Co-authored-by: zwospiel <71037447+zwospiel@users.noreply.github.com>

--- js/Brackets.js
+++ js/Brackets.js
@@ -1,50 +1,40 @@
-export class Brackets {
-    static #closedByOpen = {
-        "(": ")",
-        "[": "]",
-    }
+const closedByOpen = {
+    "(": ")",
+    "[": "]",
+}
 
-    static get #openBrackets() {
-        return Object.keys(Brackets.#closedByOpen)
-    }
+const openBrackets = Object.keys(closedByOpen)
+const closedBrackets = Object.values(closedByOpen)
+const validBrackets = openBrackets.concat(closedBrackets)
 
-    static get #closedBrackets() {
-        return Object.values(Brackets.#closedByOpen)
-    }
+/**
+ * @param {string} token
+ * @returns {boolean} True if the given token is an open bracket, false if not.
+ */
+export function isOpenBracket(token) {
+    return openBrackets.includes(token)
+}
 
-    static get #validBrackets() {
-        return Brackets.#openBrackets.concat(Brackets.#closedBrackets)
-    }
+/**
+ * @param {string} token
+ * @returns {boolean} True if the given token is an closed bracket, false if not.
+ */
+export function isClosedBracket(token) {
+    return closedBrackets.includes(token)
+}
 
-    /**
-     * @param {string} token
-     * @returns {boolean} True if the given token is an open bracket, false if not.
-     */
-    static isOpen(token) {
-        return Brackets.#openBrackets.includes(token)
-    }
+/**
+ * @param {string} token
+ * @returns {boolean} True if the given token is a valid bracket, false if not.
+ */
+export function isBracket(token) {
+    return validBrackets.includes(token)
+}
 
-    /**
-     * @param {string} token
-     * @returns {boolean} True if the given token is an closed bracket, false if not.
-     */
-    static isClosed(token) {
-        return Brackets.#closedBrackets.includes(token)
-    }
-
-    /**
-     * @param {string} openBracket
-     * @returns {string} The matching closed bracket to the given open bracket.
-     */
-    static getClosed(openBracket) {
-        return Brackets.#closedByOpen[openBracket]
-    }
-
-    /**
-     * @param {string} token
-     * @returns {boolean} True if the given token is a valid bracket, false if not.
-     */
-    static includes(token) {
-        return Brackets.#validBrackets.includes(token)
-    }
+/**
+ * @param {string} openBracket
+ * @returns {string} The matching closed bracket to the given open bracket.
+ */
+export function getClosedBracket(openBracket) {
+    return closedByOpen[openBracket]
 }
