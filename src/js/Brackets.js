67385289830539b0532d1bf22368344a61620f37
c-auri <43008483+c-auri@users.noreply.github.com>
2022-12-09 20:43:24 +0100

    Check hasBalancedBrackets in Expression

--- src/js/Brackets.js
+++ src/js/Brackets.js
@@ -32,9 +32,37 @@ export function isBracket(token) {
 }
 
 /**
+ * Verify whether the expression has balanced brackets.
+ *
+ * An expression has balanced brackets when
+ * each opening bracket is followed by a corresponding closing bracket of the same type
+ * and each closing bracket is preceded by a corresponding opening bracket of the same type.
+ * @param {string} expression
+ * @returns {boolean} True if the expression has balanced brackets, false otherwise.
+ */
+export function hasBalancedBrackets(expression) {
+    let openBrackets = []
+    for (const character of expression) {
+        if (isOpenBracket(character)) {
+            openBrackets.push(character)
+        }
+
+        if (isClosedBracket(character)) {
+            if (character === getClosedBracket(openBrackets.slice(-1)[0])) {
+                openBrackets.pop()
+            } else {
+                return false
+            }
+        }
+    }
+
+    return openBrackets.length === 0
+}
+
+/**
  * @param {string} openBracket
  * @returns {string} The matching closed bracket to the given open bracket.
  */
-export function getClosedBracket(openBracket) {
+function getClosedBracket(openBracket) {
     return closedByOpen[openBracket]
 }
